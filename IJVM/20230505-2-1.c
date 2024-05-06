7635aded8478341d0513bc947ab5aef1e3ddcfbd
Eic17H <eic17h+github@gmail.com>
2024-05-06 15:00:08 +0200

    Equivalente in C

---
+++ IJVM/20230505-2-1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int fun1(int, int);
+int fun2(int);
+
+int main(){
+    int x, y, z;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+
+    printf("%d", fun1(fun2(y+1), x+z) + fun2(x));
+    return 0;
+}
+
+int fun1(int x, int y){
+    while(x+y < 12){
+        y = y +2;
+        x = x + 1;
+    }
+    if(x%2 == 0){
+        x=x+3;
+        y=y+2;
+    }else{
+        x=x+2;
+        y=y+2;
+    }
+    return x + y;
+}
+
+int fun2(int b){
+    if(b == 0){
+        b=6;
+    }
+    if (b < 5){
+        b=5;
+    }
+    while (b%3 != 0){
+        b = ++b;
+    }
+    return b+3;
+}
