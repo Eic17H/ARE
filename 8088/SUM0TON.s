8db1bfe7861f6227c8d94708845e6bcba2e32675
Eic17 <eic17h+github@gmail.com>
2024-06-20 16:55:05 +0200

    Somma da 1 a n: ordine

--- 8088/SUM0TON.s
+++ 8088/SUM0TON.s
@@ -1,25 +1,30 @@
 ! stampa la somma dei numeri da 1 all'input
+
 _PRINTF = 127
 _GETCHAR = 117
 _EXIT = 1
 _NOOFFSET = 0x30
+
 .SECT .TEXT
 	start:
-		PUSH _GETCHAR
+			PUSH _GETCHAR
 		SYS
-		SUB AX, _NOOFFSET
+			SUB AX, _NOOFFSET
 		MOV CX, AX
 		MOV BX, 0
-	sium:
-		ADD BX, CX
-	LOOP sium
-	PUSH BX
-	PUSH s
-	PUSH _PRINTF
-	SYS
-	PUSH 0
-	PUSH _EXIT
-	SYS
+		sium:
+			ADD BX, CX
+		LOOP sium
+				PUSH BX
+				PUSH s
+			PUSH _PRINTF
+		SYS
+				PUSH 0
+			PUSH _EXIT
+		SYS
+	end:
+
 .SECT .DATA
 	s: .ASCII "Ris: %d"
+
 .SECT .BSS
