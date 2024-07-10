bef03fba930ccad476fd7dffd79e76b71373a56f
Eic17H <eic17h+github@gmail.com>
2024-07-10 18:11:59 +0200

    240628_2: finito (in teoria)

--- 8088/240628_2.s
+++ 8088/240628_2.s
@@ -62,20 +62,76 @@ _NUMOFFSET = 0x30
             JMP caso3
 
             caso1:
+                XOR AX, AX
+                MOVB AL, (SI)(BX)
+                MOVB DL, (n)
+                MULB DL ! a = v[i]*n
+                MOVB DL, BX
+                ADDB DL, 1 ! d = i+1
+                DIVB DL ! a = a/n
+                MOVB (SI)(BX), AL ! v[i] = v[i]*n/(i+1)
             JMP endif
 
             caso2:
+                XOR AX, AX
+                XOR DX, DX
+                MOVB AL, (SI)(BX) ! a = v[i]
+                ADDB AL, 4 ! a += 4
+                MOVB DL, (n)
+                DIVB DL ! a /= n
+                MOVB DL, 4(BP)
+                MULB DL ! a *= dim
+                MOVB (SI)(BX), AL
             JMP endif
 
             caso3:
+                XOR AX, AX
+                XOR DX, DX
+                MOVB AL, 4(BP) ! a = dim
+                MOVB DL, BX ! d = i
+                DIVB AL
+                MOVB DL, AL ! d = dim/i
+                MOVB AL, (SI)(DL) ! a = v[d]
+                MOVB DL, (n)
+                ADDB DL, 2 ! d = n+2
+                MULB DL ! a *= d
+                MOVB (SI)(BX), AL ! v[i] = v[dim/i]*(n+2)
             JMP endif
-        
-        endif:
-            INC BX
-            LOOP for
+            endif:
+
+        INC BX
+        LOOP for
 
         POP BP
         RET
+
+    print:
+        PUSH BP
+        MOV BP, SP
+
+        MOV BX, 0 ! indice (i)
+        MOV SI, 6(BP) ! primo parametro (v)
+        MOV CX, 4(BP) ! secondo parametro (dim)
+
+        for:
+            XOR AX, AX
+            MOVB AL, (SI)(BX) ! a = v[i]
+            MOVB DL, 2
+            MULB DL ! a *= 2
+            MOVB DL, (n)
+            DIVB DL ! a /= n
+            ! printf("%d \0", al)
+            PUSH AL
+            PUSH output
+            PUSH _PRINTF
+            SYS
+
+        INC BX
+        LOOP for
+        
+        POP BP
+        RET
+
 
 .SECT .DATA
     v: .BYTE 5,9,6,4,8,2
